08a09cedd014c72cd2cd5d1b726d20017d3f5e5c
janrebolledo <janrebolledo63@gmail.com>
2021-09-26 14:51:42 -0700

    Carousel Color Tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,32 +43,16 @@ export const products: IProduct[] = [
         image: lightbox,
         description: "Made in the U.S.",
         url: '/api/products/lightbox'
+    },
+    {
+        id: "sign-2",
+        name: "Lightbox Sign",
+        price: 100.00,
+        image: lightbox,
+        description: "Made in the U.S.",
+        url: '/api/products/lightbox'
     }
-    //,
-    // {
-    //     id: "dragonscale",
-    //     name: "Dragon Scale Betta",
-    //     price: 35,
-    //     image: dragonScaleBettaPicture,
-    //     description: "The dragon scale betta is a rarer and higher maintenance fish. It is named by its thick white scales covering his sides that looks like dragon scale armor.",
-    //     url: '/api/products/dragonscale'
-    // },
-    // {
-    //     id: "crowntail",
-    //     name: "Crowntail Betta",
-    //     price: 7.50,
-    //     image: crowntailBettaPicture,
-    //     description: "The crowntail is pretty common, but interesting none the less. It's recognized by the shape of its tail that has an appearance of a comb.",
-    //     url: '/api/products/crowntail'
-    // },
-    // {
-    //     id: "veiltail",
-    //     name: "Veiltail Betta",
-    //     price: 5.00,
-    //     image: veiltailBettaPicture,
-    //     description: "By far the most common betta fish. You can recognize it by its long tail aiming downwards.",
-    //     url: '/api/products/veiltail'
-    // }
+
 ]
 
 export const getStaticProps: GetStaticProps = async (context) => {
