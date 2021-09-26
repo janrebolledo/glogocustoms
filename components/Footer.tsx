53fdafc7483bc67abdbb1adbb164b0b5b4f7cf7e
janrebolledo <janrebolledo63@gmail.com>
2021-09-25 17:15:27 -0700

    Header + Footer changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,9 @@ export default function Footer(){
         <footer className={styles.footer}>
             <div className={styles.footer__container}>
                 <Link href="/">
+                    <div className={styles.footer__logo_container}>
                     <Image src={logo} alt="" width="120" height="120" />
+                    </div>
                 </Link>
                 <div className={styles.footer__list}>
                     <h5>Navigation</h5>
@@ -21,7 +23,7 @@ export default function Footer(){
                 </div>
                 <div className={styles.footer__list}>
                     <h5>Follow us on social media</h5>
-                    <div>
+                    <div className={styles.footer__socials}>
                         <Link href="/">
                             <a className={styles.footer__social_link} href="">
                             <Image className={styles.footer__social} src={instagram} alt="" width="32" height="32" />
