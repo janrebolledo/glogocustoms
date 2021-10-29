3128e1b10ae8607bf296a6419af9608607dd136d
janrebolledo <janrebolledo63@gmail.com>
2021-10-28 21:24:18 -0700

    Modal remove

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,22 +1,28 @@
-import React, { useState } from 'react';
-import styles from '../styles/Modal.module.scss';
-
-export default function Modal() {
-  const [isActive, setActive] = useState("false");
-
-  const handleToggle = () => {
-    setActive(!isActive);
-  };
-    return (
-      <div className={`modal ${isActive ? "" : "modal-closed"}`}>
-        <div className={styles.modal__container}>
-          <div className={styles.modal__text}>
-            <h3 className={styles.modal__heading}>Support a creator</h3>
-            <h1>GET 5% OFF</h1>
-            <p className={styles.modal__paragraph}>Enter a creator code at checkout. View all the partners <a href="/faq">here</a>.</p>
-          </div>
-          <button className={styles.modal__button} onClick={handleToggle}>✖</button>
-        </div>
-      </div>
-    )
+export default function modal() {
+  return (
+    <div></div>
+  )
 }
+
+// import React, { useState } from 'react';
+// import styles from '../styles/Modal.module.scss';
+
+// export default function Modal() {
+//   const [isActive, setActive] = useState("false");
+
+//   const handleToggle = () => {
+//     setActive(!isActive);
+//   };
+//     return (
+//       <div className={`modal ${isActive ? "" : "modal-closed"}`}>
+//         <div className={styles.modal__container}>
+//           <div className={styles.modal__text}>
+//             <h3 className={styles.modal__heading}>Support a creator</h3>
+//             <h1>GET 5% OFF</h1>
+//             <p className={styles.modal__paragraph}>Enter a creator code at checkout. View all the partners <a href="/faq">here</a>.</p>
+//           </div>
+//           <button className={styles.modal__button} onClick={handleToggle}>✖</button>
+//         </div>
+//       </div>
+//     )
+// }
