89fc904e084adcadf206350550a7990987ae45d1
janrebolledo <janrebolledo63@gmail.com>
2021-12-28 16:58:56 -0800

    Snipcart

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,8 @@ function MyApp({Component, pageProps}: AppProps) {
         <Component {...pageProps} />
         <Footer/>
         <script async src="https://cdn.snipcart.com/themes/v3.3.0/default/snipcart.js"></script>
-        <div hidden id="snipcart" data-api-key="OGI5YWJiY2MtNjBhMC00YmFlLWIwZDgtNGQwODljMTk3MjJhNjM3Njc1MzM3MjEzOTQ5NTY4"></div>
+        <div id="snipcart" data-config-modal-style="side" data-api-key="OGI5YWJiY2MtNjBhMC00YmFlLWIwZDgtNGQwODljMTk3MjJhNjM3Njc1MzM3MjEzOTQ5NTY4" hidden></div>
+        {/* Microanalytics */}
         <script data-host="https://microanalytics.io" data-dnt="false" src="https://microanalytics.io/js/script.js" id="ZwSg9rf6GA" async defer></script>
     </div>
 }
