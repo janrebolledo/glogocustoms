a0d3596d7ad39269cb7183d6a65ce2b6d331d427
janrebolledo <janrebolledo63@gmail.com>
2021-12-28 16:48:13 -0800

    Snipcart

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,20 @@ import "../styles/globals.scss";
 import type {AppProps} from 'next/app'
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Head from "next/head";
 
 function MyApp({Component, pageProps}: AppProps) {
     return <div className="app">
+        <Head>
+            <link rel="preconnect" href="https://app.snipcart.com" />
+            <link rel="preconnect" href="https://cdn.snipcart.com" />
+            <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.3.0/default/snipcart.css" />
+            <script async src="https://cdn.snipcart.com/themes/v3.3.0/default/snipcart.js"></script>
+            <div hidden id="snipcart" data-api-key="OGI5YWJiY2MtNjBhMC00YmFlLWIwZDgtNGQwODljMTk3MjJhNjM3Njc1MzM3MjEzOTQ5NTY4"></div>
+        </Head>
         <Header/>
         <Component {...pageProps} />
         <Footer/>
-        <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.2.2/default/snipcart.css" />
-        <script async src="https://cdn.snipcart.com/themes/v3.2.2/default/snipcart.js"></script>
-        <div id="snipcart" data-config-modal-style="side" data-api-key="OGI5YWJiY2MtNjBhMC00YmFlLWIwZDgtNGQwODljMTk3MjJhNjM3Njc1MzM3MjEzOTQ5NTY4" hidden></div>
         <script data-host="https://microanalytics.io" data-dnt="false" src="https://microanalytics.io/js/script.js" id="ZwSg9rf6GA" async defer></script>
     </div>
 }
