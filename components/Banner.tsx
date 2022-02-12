8b90be2fe4d96600dced1279b4384fc6953373f3
janrebolledo <janrebolledo63@gmail.com>
2022-02-12 15:46:20 -0800

    Valentine's Day Update

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,11 +1,11 @@
 import styles from "../styles/Banner.module.scss";
 
 export default function Banner() {
-    return (
-        <>
-        <div className={styles.banner}>
-        <p>FREE SHIPPING FOR LAUNCH WEEK</p>
-        </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.banner}>
+        <p>FREE SHIPPING + 20% OFF FOR VALENTINE'S DAY</p>
+      </div>
+    </>
+  );
 }
